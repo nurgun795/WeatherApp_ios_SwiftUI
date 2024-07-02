6c34f3f79407901df7a8f6464b15ad0a5a7dc2f5
Nurgun <nurgun.makarov795@gmail.com>
2024-07-02 13:51:17 +0700

    Initial Commit

---
+++ Weather2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Weather2
+//
+//  Created by nurgun on 7/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weather2/Weather2App.swift
@@ -0,0 +1,17 @@
+//
+//  Weather2App.swift
+//  Weather2
+//
+//  Created by nurgun on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Weather2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
