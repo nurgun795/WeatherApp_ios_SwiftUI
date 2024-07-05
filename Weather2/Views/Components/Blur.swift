fd516db75c905e08dcce338bf92415b0ea081115
Nurgun <nurgun.makarov795@gmail.com>
2024-07-05 13:48:30 +0700

    first commit

---
+++ Weather2/Views/Components/Blur.swift
@@ -0,0 +1,36 @@
+//
+//  Blur.swift
+//  Weather2
+//
+//  Created by nurgun on 7/2/24.
+//
+
+import SwiftUI
+
+class UIBackdropView: UIView {
+    override class var layerClass: AnyClass {
+        NSClassFromString("CABackdropLayer") ?? CALayer.self
+    }
+}
+
+struct Backdrop: UIViewRepresentable {
+    func makeUIView(context: Context) -> UIBackdropView {
+        UIBackdropView()
+    }
+    func updateUIView(_ uiView: UIBackdropView, context: Context) {}
+}
+
+struct Blur: View {
+    var radius: CGFloat = 3
+    var opaque: Bool = false
+    var body: some View {
+        Backdrop()
+            .blur(radius: radius, opaque: opaque)
+    }
+}
+
+struct Blur_Previews: PreviewProvider {
+    static var previews: some View {
+        Blur()
+    }
+}
